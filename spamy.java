3f96c391e7b3faa03e6e206f2e2870adf5f34329
fire9dragon <168269207+fire9dragon@users.noreply.github.com>
2024-08-06 15:12:25 +0200

    Create spamy.java

---
+++ spamy.java
@@ -0,0 +1,29 @@
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+public class spamy {
+
+    public static void main(String[] args) throws Exception {
+        Robot robot = new Robot();
+        robot.setAutoDelay(100); // Set a delay between key presses (in milliseconds)
+
+        
+        Thread.sleep(9000);
+
+        
+
+        
+        while (true) {
+           
+
+            robot.keyPress(17);
+            Thread.sleep(25);
+            robot.keyPress(86);
+            robot.keyRelease(86);
+            robot.keyRelease(17);
+            Thread.sleep(17);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            Thread.sleep(10);
+        }
+    }
+}
